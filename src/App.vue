07770392044d2b314d01f31fce3c3547a9963e1b
jdfrozen <616769416@qq.com>
2019-05-01 08:37:19 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,23 @@
 </template>
 
 <script>
-  export default {
-    name: 'App',
-  }
+export default {
+  name: 'App'
+}
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #484a4d;
+  /* background:  #b6bbbb21; */
+  position: absolute;
+  top: 0px;
+  bottom: 0px;
+  left: 0px;
+  right: 0px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,23 +1,24 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import App from './App'
 import router from './router'
-
-// 导入 ElementUI
+import api from './http'
+import i18n from './i18n'
+import store from './store'
+import global from '@/utils/global'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import 'font-awesome/css/font-awesome.min.css'
+import '@/assets/iconfont/iconfont.css'
 
-import App from './App'
+Vue.use(ElementUI)
+Vue.use(api)
 
-// 安装路由
-Vue.use(VueRouter);
-
-// 安装 ElementUI
-Vue.use(ElementUI);
+Vue.prototype.global = global
 
 new Vue({
   el: '#app',
-  // 启用路由
+  i18n,
   router,
-  // 启用 ElementUI
+  store,
   render: h => h(App)
 });
